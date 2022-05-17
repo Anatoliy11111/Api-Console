36a567d72b70f1651c8b011c261ca1a1401734e3
Anatoliy11111 <canatolij@list.ru>
2022-05-17 23:18:55 +0300

    create input component

---
+++ src/components/common/FieldInput/FieldInput.jsx
@@ -0,0 +1,22 @@
+import React, { memo } from 'react';
+
+import style from './FieldInput.module.css';
+
+export const FieldInput = memo(
+  ({ error, nameField, value, onChangeFieldInput, type, id, name, onBlurInput }) => (
+    <div className={style.inputContainer}>
+      <div className={error ? style.errorNameFieldInput : style.nameFieldInput}>
+        {nameField}
+      </div>
+      <input
+        id={id}
+        name={name}
+        onBlur={onBlurInput}
+        className={error ? style.errorInput : style.input}
+        value={value}
+        onChange={onChangeFieldInput}
+        type={type}
+      />
+    </div>
+  ),
+);
