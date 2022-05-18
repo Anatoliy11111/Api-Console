b37b619e2ea174291eff8783b9b6d499a2363ee2
Anatoliy11111 <canatolij@list.ru>
2022-05-18 12:39:15 +0300

    создание навигации после логина на страницу консоли

---
+++ src/components/Routes/RoutesMenu.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Route, Routes } from 'react-router-dom';
+
+import { ConsolePage } from 'components/ConsolePage/ConsolePage';
+import { LoginPage } from 'components/Login/LoginPage';
+
+export const RoutesMenu = () => (
+  <Routes>
+    <Route path="/" element={<LoginPage />} />
+    <Route path="/consolePage" element={<ConsolePage />} />
+    <Route path="*" element={<LoginPage />} />
+    {/* <Route path="consolePage" element={< />} /> */}
+  </Routes>
+);
---
+++ src/components/index.js
@@ -0,0 +1,3 @@
+export { LoginPage } from './Login/LoginPage';
+export { ConsolePage } from './ConsolePage/ConsolePage';
+export { RoutesMenu } from './Routes/RoutesMenu';
