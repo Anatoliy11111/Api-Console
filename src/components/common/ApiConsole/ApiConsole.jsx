ea5c8d0513653b1d36ed6e192392bb53a47e142d
Anatoliy11111 <canatolij@list.ru>
2022-05-18 18:40:48 +0300

    add onChange in the component ApoConsole.jsx

--- src/components/common/ApiConsole/ApiConsole.jsx
+++ src/components/common/ApiConsole/ApiConsole.jsx
@@ -2,8 +2,19 @@ import React from 'react';
 
 import style from './ApiConsole.module.css';
 
-export const ApiConsole = () => (
-  <div>
-    <textarea className={style.api_console} />
-  </div>
-);
+export const ApiConsole = ({ valueField, setValueField }) => {
+  const onChangeValueField = e => {
+    if (setValueField) {
+      setValueField(e.currentTarget.value);
+    }
+  };
+  return (
+    <div>
+      <textarea
+        className={style.api_console}
+        value={valueField}
+        onChange={e => onChangeValueField(e)}
+      />
+    </div>
+  );
+};
