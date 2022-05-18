6be479aed955b14e72a3d5f7583a27e77b65f27e
Anatoliy11111 <canatolij@list.ru>
2022-05-18 18:39:07 +0300

    create feature for format string

--- src/components/ConsolePage/ConsolePage.jsx
+++ src/components/ConsolePage/ConsolePage.jsx
@@ -1,22 +1,59 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 
 import style from './ConsolePage.module.css';
 
 import { ApiConsole, ButtonSend } from 'components/common';
 
-export const ConsolePage = () => (
-  <div className={style.ConsolePage}>
-    <h1 className={style.ConsolePage_header}> Api-консолька</h1>
-    <div className={style.ConsolePage_request} />
-    <div className={style.ConsolePage_console}>
-      <ApiConsole />
-      <div>()</div>
-      <ApiConsole />
+export const ConsolePage = () => {
+  const [valueRequestField, setValueRequestField] = useState('');
+  const onClickFormat = useCallback(() => {
+    const validValue = ['{', '}', ','];
+    const newString = [];
+    const str = valueRequestField.split('');
+
+    for (let i = 0; i < str.length; i++) {
+      if (str[i] === validValue[0] || str[i] === validValue[2]) {
+        newString.push(str[i]);
+        newString.push('\n');
+      } else if (str[i] === validValue[1]) {
+        newString.push('\n');
+        newString.push(str[i]);
+      } else {
+        newString.push(str[i]);
+      }
+    }
+    setValueRequestField(newString.join(''));
+  }, [valueRequestField]);
+
+  return (
+    <div className={style.ConsolePage}>
+      <h1 className={style.ConsolePage_header}> Api-консолька</h1>
+      <div className={style.ConsolePage_request} />
+      <div className={style.ConsolePage_console}>
+        <ApiConsole valueField={valueRequestField} setValueField={setValueRequestField} />
+        <div>()</div>
+        <ApiConsole valueField={valueRequestField} />
+      </div>
+      <div className={style.ConsolePage_footer}>
+        <ButtonSend name="Отправить" />
+        <p>@link-to-your-github</p>
+        <p role="none" onClick={onClickFormat}>
+          Форматировать
+        </p>
+      </div>
     </div>
-    <div className={style.ConsolePage_footer}>
-      <ButtonSend name="Отправить" />
-      <p>@link-to-your-github</p>
-      <p>Форматировать</p>
-    </div>
-  </div>
-);
+  );
+};
+// const sendsay = new Sendsay({
+//   apiUrl: 'https://api.sendsay.ru/general/api/v100/json',
+//   auth: {
+//     login: 'canatolij@list.ru',
+//     sublogin: 'x_1652800416165752',
+//     password: 'jo4Joogee',
+//   },
+// });
+// useEffect(() => {
+//   sendsay.request({ action: 'track.get' }).then(res => {
+//     console.log(res);
+//   });
+// }, [sendsay]);
